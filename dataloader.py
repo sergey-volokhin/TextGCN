methods sorted, moved to and from the model; major revision of metrics calculation: speedup all and fixed ndcg

dataloader.py
@@ -25,7 +25,7 @@ class DataLoader(object):
         self._get_numbers()
         self._print_info()
         self._construct_embeddings()
-        self._construct_graph()
+        self._construct_graphs()
 
         self.batch_size = min(args.batch_size, self.n_train)
         self.num_batches = (self.n_train - 1) // self.batch_size + 1
@@ -37,7 +37,7 @@ class DataLoader(object):
         self.kg_df_text = pd.read_table(f'{self.path}/kg_readable.tsv')[['asin', 'relation', 'attribute']]
         self.user_mapping = pd.read_csv(f'{self.path}/user_list.txt', sep=' ')[['org_id', 'remap_id']]
         self.item_mapping = pd.read_csv(f'{self.path}/item_list.txt', sep=' ')[['org_id', 'remap_id']]
-        self.item_mapping['remap_id'] += max(self.user_mapping['remap_id']) + 1
+        self.user_mapping['remap_id'] += max(self.item_mapping['remap_id']) + 1
         self.train_user_dict = self.train_df.groupby('user_id')['asin'].apply(np.array).to_dict()
         self.test_user_dict = self.test_df.groupby('user_id')['asin'].apply(np.array).to_dict()
 
@@ -72,7 +72,7 @@ class DataLoader(object):
         with torch.no_grad():
             self.entity_embeddings.weight[self.item_mapping['remap_id']] = embed_text(self.item_mapping['text'].to_list(), *init_bert(self.args))
 
-    def _construct_graph(self):
+    def _construct_graphs(self):
         ''' create bipartite graph with initial vectors '''
         self.graph = dgl.heterograph({('user', 'bought', 'item'): (self.train_df['user_id'].values, self.train_df['asin'].values),
                                       ('item', 'bought_by', 'user'): (self.train_df['asin'].values, self.train_df['user_id'].values)})

metric.py
@@ -1,11 +1,5 @@
-import heapq
-import multiprocessing
-from collections import defaultdict
-from itertools import repeat
-
 import numpy as np
 import torch
-from tqdm import tqdm
 
 
 def l2_loss_mean(x):
@@ -13,97 +7,31 @@ def l2_loss_mean(x):
     return torch.mean(torch.sum(torch.pow(x, 2), dim=1, keepdim=False) / 2.)
 
 
-def hit_at_k(r, k):
-    r = np.array(r)[:k]
-    if np.sum(r) > 0:
-        return 1
-    return 0
+def hit(row):
+    return row['intersecting_items'].shape[0] > 0
 
 
-def recall_at_k(hit, k, all_pos_num):
-    hit = np.asfarray(hit)[:k]
-    return np.sum(hit) / all_pos_num
+def recall(row):
+    return row['intersecting_items'].shape[0] / row['y_test'].shape[0]
 
 
-def precision_at_k(hit, k):
-    """
-    calculate Precision@k
-    hit: list, element is binary (0 / 1)
-    """
-    hit = np.asarray(hit)[:k]
-    return np.mean(hit)
+def precision(row):
+    return row['intersecting_items'].shape[0] / row['y_pred'].shape[0]
 
 
-def dcg_at_k(rel, k):
-    """
-    calculate discounted cumulative gain (dcg)
-    rel: list, element is positive real values, can be binary
-    """
-    rel = np.asfarray(rel)[:k]
+def dcg(rel):
     return np.sum((2 ** rel - 1) / np.log2(np.arange(2, rel.size + 2)))
 
 
-def ndcg_at_k(rel, k):
-    """
-    calculate normalized discounted cumulative gain (ndcg)
-    rel: list, element is positive real values, can be binary
-    """
-    idcg = dcg_at_k(sorted(rel, reverse=True), k)
-    if not idcg:
-        return 0.
-    return dcg_at_k(rel, k) / idcg
+def ndcg(row, k):
 
+    # ideal DCG when all relevant (test) items are first
+    idcg = dcg(np.concatenate([np.ones(min(k, row['y_test'].shape[0],)),
+                               np.zeros(max(0, k - row['y_test'].shape[0]))]))
 
-def test_one_user(rating, training_items, user_pos_test, n_items, ks):
+    # real DCG
+    rel = np.zeros(k)
+    rel[np.where(np.isin(row['y_pred'][:k], row['intersecting_items']))] = 1
+    numerator = dcg(rel)
 
-    all_items = set(range(n_items))
-    test_items = list(all_items - set(training_items))
-    item_score = {i: rating[i] for i in test_items}
-    k_max_item_score = heapq.nlargest(max(ks), item_score, key=item_score.get)
-    user_pos_test = set(user_pos_test)
-    rankedlist = [int(i in user_pos_test) for i in k_max_item_score]
-
-    return {
-        'recall': np.array([recall_at_k(rankedlist, K, len(user_pos_test)) for K in ks]),
-        'precision': np.array([precision_at_k(rankedlist, K) for K in ks]),
-        'hit_ratio': np.array([hit_at_k(rankedlist, K) for K in ks]),
-        'ndcg': np.array([ndcg_at_k(rankedlist, K) for K in ks]),
-    }
-
-
-def calculate_metrics_cpu(embedding, train_user_dict, test_user_dict, all_item_id_range, batch_size, ks):
-    result = defaultdict(lambda: np.zeros(len(ks)))
-    user_ids_batches = [list(test_user_dict)[i: i + batch_size] for i in range(0, len(test_user_dict), batch_size)]
-    n_users = len(test_user_dict)
-    cores = multiprocessing.cpu_count() - 1
-    pool = multiprocessing.Pool(cores)
-    with torch.no_grad():
-        for user_ids_batch in tqdm(user_ids_batches, dynamic_ncols=True, leave=False, desc='evaluating'):
-            cf_scores_batch = torch.matmul(embedding[user_ids_batch], embedding[all_item_id_range].transpose(0, 1))
-            cf_scores_batch = cf_scores_batch.cpu()
-            train_dict_batch = [train_user_dict[u] for u in user_ids_batch]
-            test_dict_batch = [test_user_dict[u] for u in user_ids_batch]
-            batch_result = pool.starmap(test_one_user, zip(cf_scores_batch, train_dict_batch, test_dict_batch, all_item_id_range, repeat(ks)))
-            for re in batch_result:
-                for i in re:
-                    result[i] += re[i] / n_users
-    pool.close()
-    return result
-
-
-def calculate_metrics(embedding, train_user_dict, test_user_dict, all_item_id_range, ks):
-    result = defaultdict(lambda: np.zeros(len(ks)))
-    n_users = len(test_user_dict)
-    with torch.no_grad():
-        for u_id, pos_item_l in tqdm(test_user_dict.items(), dynamic_ncols=True, leave=False, desc='evaluating'):
-            score = torch.matmul(embedding[u_id], embedding[all_item_id_range].transpose(0, 1))
-            score[train_user_dict[u_id]] = 0.0
-            _, rank_indices = torch.topk(score, k=max(ks), largest=True)
-            rank_indices = rank_indices.cpu().numpy()
-            rankedlist = [int(i in pos_item_l) for i in rank_indices]
-            for ind, K in enumerate(ks):
-                result['recall'][ind] += recall_at_k(rankedlist, K, len(pos_item_l)) / n_users
-                result['precision'][ind] += precision_at_k(rankedlist, K) / n_users
-                result['hit_ratio'][ind] += hit_at_k(rankedlist, K) / n_users
-                result['ndcg'][ind] += ndcg_at_k(rankedlist, K) / n_users
-    return result
+    return numerator / idcg

model.py
@@ -10,8 +10,8 @@ import torch.nn.functional as F
 import torch.optim as optim
 from tqdm import tqdm, trange
 
-from metric import calculate_metrics, l2_loss_mean
-from utils import early_stop, report_best_scores
+from metric import hit, l2_loss_mean, ndcg, precision, recall
+from utils import early_stop, save_code
 
 
 class MarcusGATConv(nn.Module):
@@ -48,8 +48,8 @@ class MarcusGATConv(nn.Module):
             graph.ndata['e_u/i'] = {'user': h_src, 'item': h_dst}
             graph['bought'].apply_edges(fn.u_dot_v('e_u/i', 'e_u/i', 'alpha'))
             graph['bought'].edata['alpha'] /= self._in_feats ** (1 / 2)
-            alpha = graph.edata.pop('alpha')
-            graph['bought_by'].edata['alpha'] = graph['bought'].edata['alpha'] = torch.softmax(alpha[('user', 'bought', 'item')], dim=0)
+            soft_alpha = torch.softmax(graph.edata.pop('alpha')[('user', 'bought', 'item')], dim=0)
+            graph['bought_by'].edata['alpha'] = graph['bought'].edata['alpha'] = soft_alpha
 
             ''' get user and item vectors updated '''
             # get layer based on the type of vertex
@@ -59,8 +59,8 @@ class MarcusGATConv(nn.Module):
             feat_dst = self.activation(feat_dst)  # g(W_2 * e_j + b_2)
             graph.ndata['g(We+b)'] = {'user': feat_src, 'item': feat_dst}
 
-            graph['bought'].update_all(fn.u_mul_e('g(We+b)', 'alpha', 'm'), fn.sum('m', 'e_new'))  # sum over item-neighbors
-            graph['bought_by'].update_all(fn.u_mul_e('g(We+b)', 'alpha', 'm'), fn.sum('m', 'e_new'))  # sum over user-neighbors
+            graph['bought'].update_all(fn.u_mul_e('g(We+b)', 'alpha', 'm'), fn.sum('m', 'e_new'))  # sum att over item-neighbors
+            graph['bought_by'].update_all(fn.u_mul_e('g(We+b)', 'alpha', 'm'), fn.sum('m', 'e_new'))  # sum att over user-neighbors
 
             rst = self.mess_drop(torch.cat([graph.ndata['e_new']['user'], graph.ndata['e_new']['item']]))
             if get_attention:
@@ -79,18 +79,13 @@ class Model(nn.Module):
         self.load_model()
         self.metrics_logger = defaultdict(lambda: np.zeros((0, len(args.k))))
         self.current_epoch = 0
+        self.sort_metric = 'recall'
 
         self.to(self.device)
         self._build_optimizer()
-        self.logger.info(args)
         # self._save_code()
+        # self.logger.info(args)
         # self.logger.info(self)
-
-    def _save_code(self):
-        ''' saving the code version that is running to the folder with the model for debugging '''
-        folder = os.path.dirname(os.path.abspath(__file__))
-        for file in ['dataloader.py', 'main.py', 'model.py', 'utils.py']:
-            os.system(f'cp {folder}/{file} {self.save_path}')
 
     def _copy_args(self, args):
         self.k = args.k
@@ -111,7 +106,7 @@ class Model(nn.Module):
         self.graph = dataset.graph
         self.logger = dataset.logger
         self.device = dataset.device
-        self.n_entities = dataset.n_entities
+        self.n_users = dataset.n_users
         self.test_user_dict = dataset.test_user_dict
         self.train_user_dict = dataset.train_user_dict
         self.entity_embeddings = dataset.entity_embeddings
@@ -148,69 +143,10 @@ class Model(nn.Module):
         reg_loss = l2_loss_mean(src_vec) + l2_loss_mean(pos_dst_vec) + l2_loss_mean(neg_dst_vec)
         return loss + self.reg_lambda * reg_loss
 
-    def evaluate(self):
-        self.eval()
-        with torch.no_grad():
-            ret = calculate_metrics(self.gnn(),  # embeddings
-                                    self.train_user_dict,
-                                    self.test_user_dict,
-                                    self.graph.ndata['id']['item'],
-                                    self.k)
-        self.logger.info(' ' * 11 + ''.join([f'@{i:<6}' for i in self.k]))
-        for i in ret:
-            self.metrics_logger[i] = np.append(self.metrics_logger[i], [ret[i]], axis=0)
-            self.logger.info(f'{i:11}' + ' '.join([f'{j:.4f}' for j in ret[i]]))
-        self.save_progression()
-        return ret
-
-    def save_progression(self):
-        ''' save all scores in a file '''
-        epochs_string, at_string = [' ' * 9], [' ' * 9]
-        width = max(10, len(self.k) * 7 - 1)
-        for i in range(len(self.metrics_logger['recall'])):
-            epochs_string.append(f'%-{width}s' % f'{(i + 1) * self.evaluate_every} epochs')
-            at_string.append(f'%-{width}s' % ' '.join([f'@{i:<5}' for i in self.k]))
-        progression = [f'Model {self.uid}', 'Full progression:', '  '.join(epochs_string), '  '.join(at_string)]
-        for k, v in self.metrics_logger.items():
-            progression.append(f'{k:11}' + '  '.join([f'%-{width}s' % ' '.join([f'{g:.4f}' for g in j]) for j in v]))
-        open(self.progression_path, 'w').write('\n'.join(progression))
-
-    def load_model(self):
-        ''' load torch weights from file '''
-        if self.load_path:
-            self.load_state_dict(torch.load(self.load_path))
-            self.logger.info(f'Loaded model {self.uid}')
-        else:
-            self.logger.info(f'Created model {self.uid}')
-        index = max([0] + [int(i[12:-4]) for i in os.listdir(self.save_path) if i.startswith('progression_')])
-        self.progression_path = f'{self.save_path}/progression_{index + 1}.txt'
-
-    def checkpoint(self):
-        ''' save current model and update the best one '''
-        os.system(f'rm -f {self.save_path}/model_checkpoint*')
-        torch.save(self.state_dict(), f'{self.save_path}/model_checkpoint{self.current_epoch}')
-        if max(self.metrics_logger['recall'][:, 0]) == self.metrics_logger['recall'][-1][0]:
-            self.logger.info(f'Updating best model at epoch {self.current_epoch}')
-            torch.save(self.state_dict(), f'{self.save_path}/model_best')
-
-    def predict(self):
-        ''' saves a tsv file with max(self.k) predictions per user along with true test items '''
-        result = []
-        embedding = self.gnn()
-        with torch.no_grad():
-            for u_id, pos_item_l in tqdm(self.test_user_dict.items(), dynamic_ncols=True, leave=False, desc='predicting'):
-                score = torch.matmul(embedding[u_id], embedding[self.graph.ndata['id']['item']].transpose(0, 1))
-                score[self.train_user_dict[u_id]] = 0.0
-                _, rank_indices = torch.topk(score, k=max(self.k), largest=True)
-                rank_indices = rank_indices.cpu().numpy()
-                result.append([rank_indices.tolist(), pos_item_l.tolist()])
-        prediction = pd.DataFrame(result, columns=['predicted', 'true_test'])
-        prediction.to_csv(f'{self.save_path}/predictions.tsv', sep='\t', index=False)
-        self.logger.info(f'Predictions are saved in {self.save_path}/predictions.tsv')
-
     def workout(self):
         ''' because torch models already have internal .train method '''
         for epoch in trange(1, self.epochs + 1, desc='epochs', dynamic_ncols=True):
+            self.current_epoch += 1
             self.train()
             loss = self.step()
             if epoch % self.evaluate_every != 0:
@@ -223,13 +159,12 @@ class Model(nn.Module):
             if early_stop(self.metrics_logger):
                 self.logger.warning(f'Early stopping triggerred at epoch {epoch}')
                 break
+
         if self.save_model:
-            self.logger.info(f'Best model is saved in `{self.save_path}/model_best`')
             self.checkpoint()
-        report_best_scores(self)
+        self.report_best_scores()
 
     def step(self):
-        self.current_epoch += 1
         loss_total = 0
         for arguments in tqdm(self.dataset.sampler(),
                               desc='current batch',
@@ -246,3 +181,87 @@ class Model(nn.Module):
             raise ValueError('loss is nan while training')
         self.scheduler.step(loss_total)
         return loss_total
+
+    def evaluate(self):
+        ''' calculate and report metrics against testset '''
+        self.eval()
+        ret = self.calculate_metrics()
+        self.logger.info(' ' * 11 + ''.join([f'@{i:<6}' for i in self.k]))
+        for i in ret:
+            self.metrics_logger[i] = np.append(self.metrics_logger[i], [ret[i]], axis=0)
+            self.logger.info(f'{i:11}' + ' '.join([f'{j:.4f}' for j in ret[i]]))
+        self.save_progression()
+        return ret
+
+    def predict(self):
+        ''' returns a pd.DataFrame with predicted and true test items for each user '''
+        predictions = []
+        embedding = self.gnn()
+        with torch.no_grad():  # don't calculate gradient since we only predict given weights
+            for u_id, pos_item_l in tqdm(self.test_user_dict.items(), dynamic_ncols=True, leave=False, desc='predicting'):
+                score = torch.matmul(embedding[u_id], embedding[self.graph.ndata['id']['item']].transpose(0, 1))
+                score[self.train_user_dict[u_id]] = np.NINF  # resetting scores for train items so we don't "predict" them
+                _, rank_indices = torch.topk(score, k=max(self.k), largest=True)
+                rank_indices = rank_indices.cpu().numpy()
+                predictions.append([rank_indices, pos_item_l])
+        return pd.DataFrame(predictions, columns=['y_pred', 'y_test'])
+
+    ''' UTILS: saving, loading, reporting scores'''
+
+    def calculate_metrics(self):
+        ''' calculate metrics for test_user_dict against predictions '''
+        predictions = self.predict()
+        result = defaultdict(lambda: np.zeros(len(self.k)))
+        for ind, K in enumerate(self.k):
+            predictions['intersecting_items'] = predictions.apply(lambda row: np.intersect1d(row['y_pred'][:K], row['y_test']), axis=1)
+            result['recall'][ind] = predictions.apply(recall, axis=1).mean()
+            result['precision'][ind] = predictions.apply(precision, axis=1).mean()
+            result['hit_ratio'][ind] = predictions.apply(hit, axis=1).mean()
+            result['ndcg'][ind] = predictions.apply(ndcg, k=K, axis=1).mean()
+        return result
+
+    def load_model(self):
+        ''' load torch weights from file '''
+        if self.load_path:
+            self.load_state_dict(torch.load(self.load_path))
+            self.logger.info(f'Loaded model {self.uid}')
+        else:
+            self.logger.info(f'Created model {self.uid}')
+        index = max([0] + [int(i[12:-4]) for i in os.listdir(self.save_path) if i.startswith('progression_')])
+        self.progression_path = f'{self.save_path}/progression_{index + 1}.txt'
+
+    def checkpoint(self):
+        ''' save current model and update the best one '''
+        os.system(f'rm -f {self.save_path}/model_checkpoint*')
+        torch.save(self.state_dict(), f'{self.save_path}/model_checkpoint{self.current_epoch}')
+        if self.metrics_logger[self.sort_metric][:, 0].max() == self.metrics_logger[self.sort_metric][-1][0]:
+            self.logger.info(f'Updating best model at epoch {self.current_epoch}')
+            torch.save(self.state_dict(), f'{self.save_path}/model_best')
+
+    def save_progression(self):
+        ''' save all scores in a file '''
+        epochs_string, at_string = [' ' * 9], [' ' * 9]
+        width = max(10, len(self.k) * 7 - 1)
+        for i in range(len(self.metrics_logger[self.sort_metric])):
+            epochs_string.append(f'%-{width}s' % f'{(i + 1) * self.evaluate_every} epochs')
+            at_string.append(f'%-{width}s' % ' '.join([f'@{i:<5}' for i in self.k]))
+        progression = [f'Model {self.uid}', 'Full progression:', '  '.join(epochs_string), '  '.join(at_string)]
+        for k, v in self.metrics_logger.items():
+            progression.append(f'{k:11}' + '  '.join([f'%-{width}s' % ' '.join([f'{g:.4f}' for g in j]) for j in v]))
+        open(self.progression_path, 'w').write('\n'.join(progression))
+
+    def report_best_scores(self):
+        ''' report best scores '''
+        self.metrics_logger = {k: np.array(v) for k, v in self.metrics_logger.items()}
+        idx = np.argmax(self.metrics_logger[self.sort_metric][:, 0])
+        self.logger.info(f'Best Metrics (at epoch {(idx + 1) * self.evaluate_every}):')
+        self.logger.info(' ' * 11 + ' '.join([f'@{i:<5}' for i in self.k]))
+        for k, v in self.metrics_logger.items():
+            self.logger.info(f'{k:11}' + ' '.join([f'{j:.4f}' for j in v[idx]]))
+        self.logger.info(f'Full progression of metrics is saved in `{self.progression_path}`')
+
+    def _save_code(self):
+        ''' saving the code version that is running to the folder with the model for debugging '''
+        folder = os.path.dirname(os.path.abspath(__file__))
+        for file in ['dataloader.py', 'main.py', 'model.py', 'utils.py']:
+            os.system(f'cp {folder}/{file} {self.save_path}')

utils.py
@@ -1,8 +1,8 @@
 import logging
+import os
 
 import dgl
 import matplotlib.pyplot as plt
-import networkx as nx
 import numpy as np
 import torch
 from tqdm import tqdm
@@ -22,18 +22,6 @@ def early_stop(res):
     return len(res['recall']) > 1 and all(np.allclose(m[-1], m[-2], atol=1e-4) for m in res.values())
 
 
-def report_best_scores(model):
-    ''' report best scores '''
-    model.metrics_logger = {k: np.array(v) for k, v in model.metrics_logger.items()}
-    best_rec_0 = max(model.metrics_logger['recall'][:, 0])
-    idx = list(model.metrics_logger['recall'][:, 0]).index(best_rec_0)
-    model.logger.info(f'Best Metrics (at epoch {(idx + 1) * model.evaluate_every}):')
-    model.logger.info(' ' * 11 + ' '.join([f'@{i:<5}' for i in model.k]))
-    for k, v in model.metrics_logger.items():
-        model.logger.info(f'{k:11}' + ' '.join([f'{j:.4f}' for j in v[idx]]))
-    model.logger.info(f'Full progression of metrics is saved in `{model.progression_path}`')
-
-
 def init_bert(args):
     torch.cuda.empty_cache()
     tokenizer = BertTokenizer.from_pretrained(args.bert_model, strip_accents=True)
@@ -50,7 +38,7 @@ def init_bert(args):
 def embed_text(sentences, tokenizer, model, batch_size):
     num_samples = len(sentences)
     token_batches = [sentences[j * batch_size:(j + 1) * batch_size] for j in range(num_samples // batch_size)] + \
-        [sentences[(num_samples // batch_size) * batch_size:]]
+                    [sentences[(num_samples // batch_size) * batch_size:]]
     embed_batches = []
     for batch in tqdm(token_batches, desc='tokenization', dynamic_ncols=True):
         embed_batches.append(tokenizer(batch,
@@ -59,13 +47,14 @@ def embed_text(sentences, tokenizer, model, batch_size):
                                        truncation=True,
                                        max_length=512))
     with torch.no_grad():
-        outputs = torch.cat([model(**batch).pooler_output for batch in tqdm(embed_batches,
-                            desc='embedding', dynamic_ncols=True)])
+        outputs = torch.cat([model(**batch).pooler_output for batch in tqdm(embed_batches, desc='embedding', dynamic_ncols=True)])
     return outputs
 
 
 def draw_bipartite(graph):
+    ''' draw a bipartite graph '''
+    import networkx as nx
     nx_g = dgl.to_homogeneous(graph).to_networkx().to_undirected()
-    pos = nx.drawing.layout.bipartite_layout(nx_g, range(len(nx_g.nodes())//2))
+    pos = nx.drawing.layout.bipartite_layout(nx_g, range(len(nx_g.nodes()) // 2))
     nx.draw(nx_g, pos, with_labels=True, node_color=[[.7, .7, .7]])
     plt.show()