b37922494b77e09803b8351bd1cfb02339ce7ab5
Sergey <svolokh@emory.edu>
2022-01-29 17:52:27 -0500

    deberta

--- parser.py
+++ parser.py
@@ -45,7 +45,7 @@ def parse_args():
                         help='whether to save the predictions for test set')
     parser.add_argument('--bert-model',
                         type=str,
-                        default='deberta-v3-base',
+                        default='microsoft/deberta-v3-base',
                         help='version of BERT to use')
     parser.add_argument('--gpu',
                         type=str,
