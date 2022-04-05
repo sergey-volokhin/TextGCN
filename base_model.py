switched to torch dataloader batching; refactoring base classes; centralized representation; fix NGCF formula

base_model.py
@@ -3,12 +3,11 @@ import os
 import numpy as np
 import pandas as pd
 import torch
-import torch.optim as opt
 from tensorboardX import SummaryWriter
 from torch import nn
 from tqdm import tqdm, trange
 
-from utils import early_stop, hit, minibatch, ndcg, precision, recall
+from utils import early_stop, hit, ndcg, precision, recall
 
 
 class BaseModel(nn.Module):
@@ -19,27 +18,22 @@ class BaseModel(nn.Module):
 
         self._copy_args(args)
         self._copy_dataset_args(dataset)
-        self._init_embeddings()
+        self._init_embeddings(args.emb_size)
         self._add_vars()
-        self._add_torch_vars()
 
-        self.load_model()
+        self.load_model(args.load)
         self._save_code()
         self.logger.info(args)
         self.logger.info(self)
 
     def _copy_args(self, args):
         self.k = args.k
-        self.lr = args.lr
         self.uid = args.uid
-        self.quiet = args.quiet
         self.epochs = args.epochs
         self.logger = args.logger
         self.device = args.device
-        self.load_path = args.load
-        self.emb_size = args.emb_size
+        self.dropout = args.dropout
         self.n_layers = args.n_layers
-        self.keep_prob = args.keep_prob
         self.save_path = args.save_path
         self.save_model = args.save_model
         self.batch_size = args.batch_size
@@ -50,43 +44,25 @@ class BaseModel(nn.Module):
     def _copy_dataset_args(self, dataset):
         self.n_users = dataset.n_users
         self.n_items = dataset.n_items
-        self.sampler = dataset.sampler
         self.n_batches = dataset.n_batches
         self.norm_matrix = dataset.norm_matrix
         self.test_user_dict = dataset.test_user_dict
         self.get_user_pos_items = dataset.get_user_pos_items
 
-    def _init_embeddings(self):
+    def _init_embeddings(self, emb_size):
         ''' randomly initialize entity embeddings '''
         self.embedding_user = nn.Embedding(num_embeddings=self.n_users,
-                                           embedding_dim=self.emb_size).to(self.device)
+                                           embedding_dim=emb_size).to(self.device)
         self.embedding_item = nn.Embedding(num_embeddings=self.n_items,
-                                           embedding_dim=self.emb_size).to(self.device)
+                                           embedding_dim=emb_size).to(self.device)
         nn.init.normal_(self.embedding_user.weight, std=0.1)
         nn.init.normal_(self.embedding_item.weight, std=0.1)
 
     def _add_vars(self):
-        self.current_epoch = 0
+        ''' adding all the remaining variables '''
         self.metrics = ['recall', 'precision', 'hit', 'ndcg', 'f1']
         self.metrics_logger = {i: np.zeros((0, len(self.k))) for i in self.metrics}
         self.w = SummaryWriter(self.save_path)
-
-    def _add_torch_vars(self):
-        '''
-            adding all the torch variables so we could
-            move to cuda before making optimizers
-        '''
-        pass
-
-    @property
-    def _dropout_norm_matrix(self):
-        ''' drop (1 - self.keep_prob) elements from adjacency table '''
-        index = self.norm_matrix.indices().t()
-        values = self.norm_matrix.values()
-        random_index = (torch.rand(len(values)) + self.keep_prob).int().bool()
-        index = index[random_index]
-        values = values[random_index] / self.keep_prob
-        return torch.sparse.FloatTensor(index.t(), values, self.norm_matrix.size()).to(self.device)
 
     def get_loss(self, users, pos, neg):
 
@@ -108,56 +84,7 @@ class BaseModel(nn.Module):
 
         return loss + self.reg_lambda * reg_loss
 
-    def step(self):
-        '''
-            one iteration over one sampler
-            i.e. one step of the training cycle
-            returns total loss over all users
-        '''
-        total_loss = 0
-        if self.slurm:
-            sampler = self.sampler()
-        else:
-            sampler = tqdm(self.sampler(),
-                           total=self.n_batches,
-                           desc='current batch',
-                           leave=False,
-                           dynamic_ncols=True)
-
-        for arguments in sampler:
-            self.optimizer.zero_grad()
-            loss = self.get_loss(*arguments)
-            total_loss += loss
-            loss.backward()
-            self.optimizer.step()
-
-        total_loss = total_loss.item()
-        self.w.add_scalar('Training_loss', total_loss, self.current_epoch)
-        assert not np.isnan(total_loss), 'loss is nan while training'
-        self.scheduler.step(total_loss)
-        return total_loss
-
-    def forward(self):
-        for self.current_epoch in trange(self.current_epoch + 1,
-                                         self.epochs + 1,
-                                         desc='epochs',
-                                         dynamic_ncols=True):
-            self.train()
-            loss = self.step()
-            if self.current_epoch % self.evaluate_every:
-                continue
-            self.logger.info(f'Epoch {self.current_epoch}: loss = {loss}')
-            self.evaluate()
-            self.checkpoint()
-            if early_stop(self.metrics_logger):
-                self.logger.warning(f'Early stopping triggerred at epoch {self.current_epoch}')
-                self.current_epoch = self.epochs
-                break
-        else:
-            self.checkpoint()
-        self.w.flush()
-
-    def evaluate(self):
+    def evaluate(self, epoch):
         ''' calculate and report metrics for test users against predictions '''
         self.eval()
         predictions = self.predict()
@@ -178,7 +105,7 @@ class BaseModel(nn.Module):
             results[metric] /= n_users
             self.w.add_scalars(f'Test/{metric}',
                                {str(self.k[i]): results[metric][i] for i in range(len(self.k))},
-                               self.current_epoch)
+                               epoch)
 
         ''' show metrics in log '''
         self.logger.info(' ' * 11 + ''.join([f'@{i:<6}' for i in self.k]))
@@ -200,14 +127,8 @@ class BaseModel(nn.Module):
         with torch.no_grad():  # don't calculate gradient since we only predict
             users_emb, items_emb = self.representation
 
-            if self.slurm:
-                batches = minibatch(users, batch_size=self.batch_size)
-            else:
-                batches = tqdm(minibatch(users, batch_size=self.batch_size),
-                               total=(len(users) - 1) // self.batch_size + 1,
-                               desc='test batches',
-                               leave=False,
-                               dynamic_ncols=True)
+            batches = [users[j:j + self.batch_size] for j in range(0, len(users), self.batch_size)]
+            batches = batches if self.slurm else tqdm(batches, desc='batches', leave=False, dynamic_ncols=True)
 
             for batch_users in batches:
 
@@ -260,30 +181,22 @@ class BaseModel(nn.Module):
             if file.endswith('.py'):
                 os.system(f'cp {folder}/{file} {self.save_path}/code/{file}_')
 
-    def load_model(self):
+    def load_model(self, load_path):
         ''' load torch weights from file '''
-        if self.load_path:
-            checkpoint = torch.load(self.load_path)
-            self.load_state_dict(checkpoint['model_state_dict'])
-            self.optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
-            self.current_epoch = checkpoint['current_epoch']
-            self.metrics_logger = checkpoint['metrics']
-            self.logger.info(f'Loaded model {self.load_path}')
+        if load_path:
+            self.load_state_dict(torch.load(load_path))
+            self.logger.info(f'Loaded model {load_path}')
         else:
             self.logger.info(f'Created model {self.uid}')
         self.progression_path = f'{self.save_path}/progression.txt'
 
-    def checkpoint(self):
+    def checkpoint(self, epoch):
         ''' save current model and update the best one '''
         if not self.save_model:
             return
-        torch.save({'current_epoch': self.current_epoch,
-                    'model_state_dict': self.state_dict(),
-                    'optimizer_state_dict': self.optimizer.state_dict(),
-                    'metrics': self.metrics_logger},
-                   f'{self.save_path}/checkpoint.pkl')
+        torch.save(self.state_dict(), f'{self.save_path}/checkpoint.pkl')
         if self.metrics_logger[self.metrics[0]][:, 0].max() == self.metrics_logger[self.metrics[0]][-1][0]:
-            self.logger.info(f'Updating best model at epoch {self.current_epoch}')
+            self.logger.info(f'Updating best model at epoch {epoch}')
             os.system(f'cp {self.save_path}/checkpoint.pkl {self.save_path}/best.pkl')
 
     def save_progression(self):
@@ -302,15 +215,74 @@ class BaseModel(nn.Module):
     @property
     def representation(self):
         '''
-            get the users' and items' final representations using
-            calculated embeddings and propagated through layers
+            get the users' and items' final representations
+            propagated through all the layers
+        '''
+        curent_lvl_emb_matrix = self.embedding_matrix
+        norm_matrix = self._dropout_norm_matrix
+        node_embed_cache = [curent_lvl_emb_matrix]
+        for _ in range(self.n_layers):
+            curent_lvl_emb_matrix = self.layer_propagate(norm_matrix, curent_lvl_emb_matrix)
+            node_embed_cache.append(curent_lvl_emb_matrix)
+        aggregated_embeddings = self.layer_aggregation(node_embed_cache)
+        return torch.split(aggregated_embeddings, [self.n_users, self.n_items])
+
+    def layer_propagate(self, *args, **kwargs):
+        '''
+            propagate the current layer embedding matrix
+            through and get the matrix of the next layer
         '''
         raise NotImplementedError
 
-    def torch_forward(self, args):
-        self.optimizer.zero_grad()
-        loss = self.get_loss(*args.t())
-        loss.backward()
-        self.optimizer.step()
-        self.w.add_scalar('Training_loss', loss, self.current_epoch)
-        return loss
+    def layer_aggregation(self, *args, **kwargs):
+        '''
+            given embeddings from all layers
+            combine them into final representation matrix
+        '''
+        raise NotImplementedError
+
+    def embedding_matrix(self, *args, **kwargs):
+        ''' get the embedding matrix of 0th layer '''
+        raise NotImplementedError
+
+    @property
+    def _dropout_norm_matrix(self):
+        '''
+            drop self.dropout elements from adj table
+            to help with overfitting
+        '''
+        index = self.norm_matrix.indices().t()
+        values = self.norm_matrix.values()
+        random_index = (torch.rand(len(values)) + (1 - self.dropout)).int().bool()
+        index = index[random_index]
+        values = values[random_index] / (1 - self.dropout)
+        return torch.sparse.FloatTensor(index.t(), values, self.norm_matrix.size()).to(self.device)
+
+    def forward(self, batches, optimizer, scheduler):
+        ''' training function '''
+
+        for epoch in trange(1, self.epochs + 1, desc='epochs'):
+            self.train()
+            total_loss = 0
+            batches = batches if self.slurm else tqdm(batches, desc='batches', leave=False, dynamic_ncols=True)
+            for data in batches:
+                optimizer.zero_grad()
+                loss = self.get_loss(*data.to(self.device).t())
+                total_loss += loss
+                loss.backward()
+                optimizer.step()
+            self.w.add_scalar('Training_loss', total_loss, epoch)
+            scheduler.step(total_loss)
+
+            if epoch % self.evaluate_every:
+                continue
+
+            self.logger.info(f'Epoch {epoch}: loss = {total_loss}')
+            self.evaluate(epoch)
+            self.checkpoint(epoch)
+
+            if early_stop(self.metrics_logger):
+                self.logger.warning(f'Early stopping triggerred at epoch {epoch}')
+                break
+        else:
+            self.checkpoint(self.epochs)

lightgcn.py
@@ -7,38 +7,29 @@ from base_model import BaseModel
 
 class LightGCN(BaseModel):
 
-    @property
-    def representation(self):
-        '''
-            calculate current embeddings,
-            pull the through layers and aggregate
-        '''
-        h = torch.cat([self.embedding_user.weight, self.embedding_item.weight])
-        norm_matrix = self._dropout_norm_matrix
-        node_embed_cache = [h]
-        for _ in range(self.n_layers):
-            h = self.layer_propagate(norm_matrix, h)
-            node_embed_cache.append(h)
-        aggregated_embeddings = self.layer_aggregation(node_embed_cache)
-        return torch.split(aggregated_embeddings, [self.n_users, self.n_items])
-
-    def layer_propagate(self, norm_matrix, h):
-        return torch.sparse.mm(norm_matrix, h)
+    def layer_propagate(self, norm_matrix, emb_matrix):
+        return torch.sparse.mm(norm_matrix, emb_matrix)
 
     def layer_aggregation(self, vectors):
         return torch.mean(torch.stack(vectors, dim=1), dim=1)
 
+    def embedding_matrix(self):
+        ''' get the embedding matrix of 0th layer '''
+        return torch.cat([self.embedding_user.weight, self.embedding_item.weight])
+
 
 class LightGCNAttn(LightGCN):
 
-    def _add_torch_vars(self):
-        # ngcf variables init
+    def _add_vars(self):
+        super()._add_vars()
+
+        # NGCF weights
         self.W_ngcf = nn.Parameter(torch.empty((2, 1)), requires_grad=True)
         nn.init.xavier_normal_(self.W_ngcf, gain=nn.init.calculate_gain('relu'))
 
     def layer_propagate(self, norm_matrix, h):
         '''
-            propagate messages through layer using ngcf formula
+            propagate messages through layer using NGCF attention
                                          e_i                   e_u ⊙ e_i
             e_u = σ(W1*e_u + W1*SUM---------------- + W2*SUM----------------)
                                    sqrt(|N_u||N_i|)         sqrt(|N_u||N_i|)
@@ -51,7 +42,9 @@ class LightGCNAttn(LightGCN):
 
 class LightGCNWeight(LightGCN):
 
-    def _add_torch_vars(self):
+    def _add_vars(self):
+        super()._add_vars()
+
         # linear combination of layer represenatations
         self.W_layers = nn.Parameter(torch.empty((self.n_layers + 1, 1)), requires_grad=True)
         nn.init.xavier_normal_(self.W_layers, gain=nn.init.calculate_gain('relu'))
@@ -66,5 +59,5 @@ class LightGCNSingle(LightGCN):
         return vectors[-1]
 
 
-class LightSingleGCNAttn(LightGCNSingle, LightGCNAttn):
+class LightGCNSingleAttn(LightGCNSingle, LightGCNAttn):
     pass

main.py
@@ -1,74 +1,15 @@
-import cProfile
-import pstats
-
 import numpy as np
 import torch
 import torch.optim as opt
-from rich import pretty
 from torch import nn
 from torch.utils.data import DataLoader
-from tqdm import tqdm, trange
 
-from dataloader import MyDataLoader as DataLoaderLightGCN
+from dataset import BaseDataset
 from lightgcn import (LightGCN, LightGCNAttn, LightGCNSingle, LightGCNWeight,
-                      LightSingleGCNAttn)
-from text_model import DataLoaderKG, TextModelKG
-from text_model_reviews import NGCF, DataLoaderReviews, TextModelReviews
+                      LightGCNSingleAttn)
+# from text_model import DatasetKG, TextModelKG
+from text_model_reviews import NGCF, DatasetReviews, ReviewModel, ReviewModelSingle
 from parser import parse_args
-
-pretty.install()
-
-
-def main():
-    dataset = loader_class(args)
-    model = model_class(args, dataset)
-    model()
-    if args.predict:
-        model.predict(save=True)
-
-
-def parallel_main():
-    dataset = loader_class(args)
-    loader = DataLoader(dataset, batch_size=args.batch_size, shuffle=True)
-    model = model_class(args, dataset)
-
-    model = nn.DataParallel(model, device_ids=[0])
-    model = model.to(torch.device("cuda"))
-
-    optimizer = opt.Adam(model.parameters(), lr=args.lr)
-    scheduler = opt.lr_scheduler.ReduceLROnPlateau(optimizer,
-                                                   verbose=(not args.quiet),
-                                                   patience=5,
-                                                   min_lr=1e-6)
-    for epoch in trange(1, args.epochs + 1, desc='epochs'):
-        model.train()
-        total_loss = 0
-        for data in tqdm(loader, desc='data', leave=False):
-            optimizer.zero_grad()
-            loss = model.module.get_loss(*data.to(args.device).t())
-            total_loss += loss
-            loss.backward()
-            optimizer.step()
-        model.module.w.add_scalar('Training_loss', total_loss, epoch)
-        scheduler.step(total_loss)
-
-        if epoch % args.evaluate_every:
-            continue
-
-        model.module.logger.info(f'Epoch {epoch}: loss = {total_loss}')
-        model.module.evaluate()
-        model.module.checkpoint()
-
-
-def profile(func):
-    def wrapper(*args, **kwargs):
-        profiler = cProfile.Profile()
-        profiler.enable()
-        func(*args, **kwargs)
-        profiler.disable()
-        stats = pstats.Stats(profiler).sort_stats('cumtime')
-        stats.print_stats()
-    return wrapper
 
 
 if __name__ == '__main__':
@@ -78,15 +19,32 @@ if __name__ == '__main__':
     np.random.seed(args.seed)
     torch.manual_seed(args.seed)
 
-    loader_class, model_class = {'kg': (DataLoaderKG, TextModelKG),
-                                 'lgcn': (DataLoaderLightGCN, LightGCN),
-                                 'lgcn_single': (DataLoaderLightGCN, LightGCNSingle),
-                                 'lgcn_weights': (DataLoaderLightGCN, LightGCNWeight),
-                                 'lgcn_single_attn': (DataLoaderLightGCN, LightSingleGCNAttn),
-                                 'lgcn_attn': (DataLoaderLightGCN, LightGCNAttn),
-                                 'reviews': (DataLoaderReviews, TextModelReviews),
-                                 'ngcf': (DataLoaderReviews, NGCF),
-                                 }[args.model]
+    Dataset, Model = {
+        'lgcn': (BaseDataset, LightGCN),
+        'lgcn_single': (BaseDataset, LightGCNSingle),
+        'lgcn_attn': (BaseDataset, LightGCNAttn),
+        'lgcn_single_attn': (BaseDataset, LightGCNSingleAttn),
+        'lgcn_weights': (BaseDataset, LightGCNWeight),
+        'reviews': (DatasetReviews, ReviewModel),
+        'reviews_single': (DatasetReviews, ReviewModelSingle),
+        'ngcf': (DatasetReviews, NGCF),
+        # 'kg': (DatasetKG, TextModelKG),
+    }[args.model]
 
-    parallel_main()
-    # main()
+    dataset = Dataset(args)
+    loader = DataLoader(dataset, batch_size=args.batch_size, shuffle=True)
+    model = Model(args, dataset)
+
+    model = nn.DataParallel(model, device_ids=range(len(args.gpu.split(','))))
+    model = model.to(torch.device("cuda"))
+
+    optimizer = opt.Adam(model.parameters(), lr=args.lr)
+    scheduler = opt.lr_scheduler.ReduceLROnPlateau(optimizer,
+                                                   verbose=(not args.quiet),
+                                                   patience=5,
+                                                   min_lr=1e-6)
+
+    model(loader, optimizer, scheduler)
+
+    if args.predict:
+        model.module.predict(save=True)

text_model.py
@@ -6,19 +6,17 @@ import dgl.function as fn
 import pandas as pd
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
 from tqdm import tqdm
 
 from base_model import BaseModel
-from dataloader import MyDataLoader
+from dataset import BaseDataset
 from utils import embed_text
 
 
-class DataLoaderKG(MyDataLoader):
+class DatasetKG(BaseDataset):
 
     def _copy_args(self, args):
         super()._copy_args(args)
-
         self.sep = args.sep
         self.freeze = args.freeze
         self.bert_model = args.bert_model
@@ -36,7 +34,7 @@ class DataLoaderKG(MyDataLoader):
         self.item_mapping['text'] = self.item_mapping['org_id'].map(self.item_text_dict)
 
     def _init_embeddings(self):
-        ''' construct BERT representation for items and overwrite model item embeddings with them'''
+        ''' construct BERT representation for items and overwrite model item embeddings '''
         super()._init_embeddings()
 
         emb_path = f'embeddings_{self.bert_model.split("/")[-1]}.torch'
@@ -145,19 +143,20 @@ class TextModelKG(BaseModel):
         g = self.graph.local_var()  # entering local mode to freeze the graph for computations
 
         h = torch.cat([self.embedding_user.weight, self.embedding_item.weight])
-        node_embed_cache = [h]  # we need to remove the first layer representations of the user nodes
+        # norm_matrix = self._dropout_norm_matrix
+        node_embed_cache = [h]
 
         ''' first layer processed using user_vector '''
         h = self.layers[0](g, h, self.user_vector)  # add norm_matrix multiplication
-
-        out = F.normalize(h, p=2, dim=1)
-        node_embed_cache.append(out)
+        node_embed_cache.append(h)
 
         ''' rest of the layers processed using user embeddings '''
         for layer in self.layers[1:]:
             h = layer(g, h)
-            out = F.normalize(h, p=2, dim=1)
-            node_embed_cache.append(out)
-        aggregated_embeddings = torch.cat(node_embed_cache, 1)
-
+            # h = self.layer_propagate(norm_matrix, h)
+            node_embed_cache.append(h)
+        aggregated_embeddings = self.layer_aggregation(node_embed_cache)
         return torch.split(aggregated_embeddings, [self.n_users, self.n_items])
+
+    def layer_aggregation(self, vectors):
+        return torch.cat(vectors, 1)

text_model_reviews.py
@@ -6,11 +6,11 @@ from torch import nn
 from tqdm import tqdm
 
 from base_model import BaseModel
-from dataloader import MyDataLoader
+from dataset import BaseDataset
 from utils import embed_text
 
 
-class DataLoaderReviews(MyDataLoader):
+class DatasetReviews(BaseDataset):
 
     def _load_files(self):
         super()._load_files()
@@ -27,15 +27,26 @@ class DataLoaderReviews(MyDataLoader):
                                           self.reviews.groupby('asin')['user_id'].agg(len)]).median())
 
         ''' remove extra reviews not used in representation '''
-        top_median_reviews = set()
-        for _, group in tqdm(self.reviews.groupby('user_id'), leave=False):
-            top_median_reviews |= set(group.sort_values('unixReviewTime', ascending=False)['review'].head(self.num_reviews))
-        for _, group in tqdm(self.reviews.groupby('asin'), leave=False):
-            top_median_reviews |= set(group.sort_values('unixReviewTime', ascending=False)['review'].head(self.num_reviews))
-        self.reviews = self.reviews[self.reviews['review'].isin(top_median_reviews)]
+        cut_reviews = set()
+        for _, group in tqdm(self.reviews.groupby('user_id'),
+                             leave=False,
+                             desc='selecting reviews',
+                             dynamic_ncols=True):
+            cut_reviews |= set(group.sort_values('unixReviewTime', ascending=False)['review'].head(self.num_reviews))
+        for _, group in tqdm(self.reviews.groupby('asin'),
+                             leave=False,
+                             desc='selecting reviews',
+                             dynamic_ncols=True):
+            cut_reviews |= set(group.sort_values('unixReviewTime', ascending=False)['review'].head(self.num_reviews))
+        self.reviews = self.reviews[self.reviews['review'].isin(cut_reviews)]
 
 
-class TextModelReviews(BaseModel):
+class ReviewModel(BaseModel):
+
+    def __init__(self, args, dataset):
+        super().__init__(args, dataset)
+        self.phase = 1
+        self.epochs //= 2
 
     def _copy_args(self, args):
         super()._copy_args(args)
@@ -50,23 +61,45 @@ class TextModelReviews(BaseModel):
         self.user_mapping = dataset.user_mapping
         self.item_mapping = dataset.item_mapping
 
-    def _add_torch_vars(self):
+    def _add_vars(self):
+        super()._add_vars()
+
         ''' pooling layers to combine lgcn and bert '''
         gain = nn.init.calculate_gain('relu')
 
         # linear combination of reviews' vectors
-        self.review_agg_layer = nn.Linear(in_features=self.num_reviews, out_features=1, bias=True)
+        self.review_agg_layer = nn.Linear(in_features=self.num_reviews, out_features=1, bias=True, device=self.device)
         nn.init.xavier_normal_(self.review_agg_layer.weight, gain=gain)
         nn.init.constant_(self.review_agg_layer.bias, 0.0)
 
         # linear combination of lightgcn and bert vectors
-        self.linear = nn.Linear(in_features=2, out_features=1, bias=True)
+        self.linear = nn.Linear(in_features=2, out_features=1, bias=True, device=self.device)
         nn.init.xavier_normal_(self.linear.weight, gain=gain)
         nn.init.constant_(self.linear.bias, 0.0)
 
-        # linear combination of layer represenatations
-        self.W_layers = nn.Parameter(torch.empty((self.n_layers + 1, 1)), requires_grad=True)
-        nn.init.xavier_normal_(self.W_layers, gain=gain)
+        ''' load/calc embeddings of the reviews and setup the dicts '''
+
+        # calculate reviews' embeddings
+        self.reviews['vector'] = embed_text(self.reviews['review'],
+                                            'reviews',
+                                            self.path,
+                                            self.bert_model,
+                                            self.emb_batch_size,
+                                            self.device,
+                                            self.logger)
+
+        # make a table of embedded reviews for user and item to be pooled
+        user_text_embs = {}
+        for user, group in self.reviews.groupby('user_id')['vector']:
+            user_text_embs[user] = self._pad_reviews(group.values)
+        self.user_text_embs = nn.Parameter(torch.stack(
+            [i[1] for i in sorted(user_text_embs.items())]).to(self.device))
+
+        item_text_embs = {}
+        for item, group in self.reviews.groupby('asin')['vector']:
+            item_text_embs[item] = self._pad_reviews(group.values)
+        self.item_text_embs = nn.Parameter(torch.stack(
+            [i[1] for i in sorted(item_text_embs.items())]).to(self.device))
 
     def _pad_reviews(self, reviews):
         ''' pad or cut existing reviews to have equal number per user/item '''
@@ -74,23 +107,20 @@ class TextModelReviews(BaseModel):
         return F.pad(reviews, (0, 0, 0, self.num_reviews - reviews.shape[0]))
 
     def layer_propagate(self, norm_matrix, h):
-        ''' propagate messages through layer '''
         return torch.sparse.mm(norm_matrix, h)
 
     def layer_aggregation(self, vectors):
-        ''' aggregate layer representations into one vector '''
-        return (self.W_layers.T * vectors.T).T.sum(axis=0)  # TODO could improve. attention?
-        # return torch.mean(torch.stack(vectors, dim=1), dim=1)
+        return torch.mean(torch.stack(vectors, dim=1), dim=1)
 
     @property
-    def current_embeddings(self):
+    def embedding_matrix(self):
         ''' recalculate embeddings # TODO use (users, pos, neg) to save time?
 
             for lightgcn phase:
                 take embs as is
 
             for reviews phase:
-                get linear combination of top num_reviews reviews,\\
+                get linear combination of top num_reviews reviews,
                 combine with LGCN vector using another linear layer
         '''
 
@@ -107,70 +137,21 @@ class TextModelReviews(BaseModel):
 
         return torch.cat([emb_user, emb_item])
 
-    @property
-    def representation(self):
-        '''
-            calculate current embeddings,
-            pull the through layers and aggregate
-        '''
-        norm_matrix = self._dropout_norm_matrix
-        h = self.current_embeddings
-        node_embed_cache = [h]
-        for _ in range(self.n_layers):
-            h = self.layer_propagate(norm_matrix, h)
-            node_embed_cache.append(h)
-        aggregated_embeddings = self.layer_aggregation(torch.stack(node_embed_cache))
-        return torch.split(aggregated_embeddings, [self.n_users, self.n_items])
-
-    def setup_second_phase(self):
-        '''
-            this function is in the model and not in dataloader
-            because we want to run the reliable lightgcn part first
-        '''
-
-        ''' calculate reviews' embeddings '''
-        self.reviews['vector'] = embed_text(self.reviews['review'],
-                                            'reviews',
-                                            self.path,
-                                            self.bert_model,
-                                            self.emb_batch_size,
-                                            self.device,
-                                            self.logger)
-
-        ''' make a table of embedded reviews for user and item to be pooled '''
-        user_text_embs = {}
-        user_mapping_dict = {i['org_id']: i['remap_id'] for i in self.user_mapping.to_dict(orient='records')}
-        for user, group in self.reviews.groupby('user_id')['vector']:
-            user_text_embs[user_mapping_dict[user]] = self._pad_reviews(group.values)
-        self.user_text_embs = nn.Parameter(torch.stack([i[1] for i in sorted(user_text_embs.items())]).to(self.device))
-
-        item_text_embs = {}
-        item_mapping_dict = {i['org_id']: i['remap_id'] for i in self.item_mapping.to_dict(orient='records')}
-        for item, group in self.reviews.groupby('asin')['vector']:
-            item_text_embs[item_mapping_dict[item]] = self._pad_reviews(group.values)
-        self.item_text_embs = nn.Parameter(torch.stack([i[1] for i in sorted(item_text_embs.items())]).to(self.device))
-
-    def forward(self):
-
-        if 'item_text_embs' not in self.state_dict():
-            ''' first phase: train lightgcn embs for half epochs '''
-            self.logger.info('Phase 1: Training LightGCN weights')
-            self.phase = 1
-            self.epochs //= 2
-            super().forward()
-
-        ''' second phase: pool trained with textual embeddings for half epochs '''
-        self.logger.info('Phase 2: Training with pooled textual weights')
+    def forward(self, loader, optimizer, scheduler):
+        super().forward(loader, optimizer, scheduler)
         self.phase = 2
-        self.epochs *= 2
-        self.setup_second_phase()
-        super().forward()
+        super().forward(loader, optimizer, scheduler)
 
 
-class NGCF(TextModelReviews):
+class ReviewModelSingle(ReviewModel):
+    def layer_aggregation(self, vectors):
+        return vectors[-1]
 
-    def _add_torch_vars(self):
-        super()._add_torch_vars()
+
+class NGCF(ReviewModel):
+
+    def _add_vars(self):
+        super()._add_vars()
         # ngcf variables init
         self.W_ngcf = nn.Parameter(torch.empty((2, 1)), requires_grad=True)
         nn.init.xavier_normal_(self.W_ngcf, gain=nn.init.calculate_gain('relu'))
@@ -178,11 +159,11 @@ class NGCF(TextModelReviews):
     def layer_propagate(self, norm_matrix, h):
         '''
             propagate messages through layer using ngcf formula
-                                       e_i                   e_u ⊙ e_i
-            e_u = W1*e_u + W1*SUM---------------- + W2*SUM----------------
-                                 sqrt(|N_u||N_i|)         sqrt(|N_u||N_i|)
+                                         e_i                   e_u ⊙ e_i
+            e_u = σ(W1*e_u + W1*SUM---------------- + W2*SUM----------------)
+                                   sqrt(|N_u||N_i|)         sqrt(|N_u||N_i|)
         '''
 
         summ = torch.sparse.mm(norm_matrix, h)
-        return (self.W_ngcf[0] * h) + (self.W_ngcf[0] * summ) + \
-               (self.W_ngcf[1] * torch.mul(h, summ))
+        return F.leaky_relu((self.W_ngcf[0] * h) + (self.W_ngcf[0] * summ) +
+                            (self.W_ngcf[1] * torch.mul(h, summ)))

utils.py
@@ -1,5 +1,7 @@
+import cProfile
 import logging
 import os
+import pstats
 
 import numpy as np
 import torch
@@ -24,12 +26,11 @@ def dcg(rel):
 
 
 def ndcg(row, k):
-    idcg = dcg(np.concatenate([np.ones(min(k, row['y_true'].shape[0],)),
+    idcg = dcg(np.concatenate([np.ones(min(k, row['y_true'].shape[0])),
                                np.zeros(max(0, k - row['y_true'].shape[0]))]))
     rel = np.zeros(k)
     rel[np.where(np.isin(row['y_pred'][:k], row['intersecting_items']))] = 1
-    numerator = dcg(rel)
-    return numerator / idcg
+    return dcg(rel) / idcg
 
 
 def get_logger(args):
@@ -51,26 +52,6 @@ def early_stop(res):
     return len(res['recall']) > 1 and all(np.allclose(m[-1], m[-2], atol=1e-4) for m in res.values())
 
 
-def minibatch(*tensors, **kwargs):
-    batch_size = kwargs['batch_size']
-    if len(tensors) == 1:
-        tensor = tensors[0]
-        for i in range(0, len(tensor), batch_size):
-            yield tensor[i:i + batch_size]
-    else:
-        for i in range(0, len(tensors[0]), batch_size):
-            yield tuple(x[i:i + batch_size] for x in tensors)
-
-
-def shuffle(*arrays):
-    assert len(set(len(x) for x in arrays)) == 1, 'All inputs to shuffle must have the same length.'
-    shuffle_indices = np.arange(len(arrays[0]))
-    np.random.shuffle(shuffle_indices)
-    if len(arrays) == 1:
-        return arrays[0][shuffle_indices]
-    return tuple(x[shuffle_indices] for x in arrays)
-
-
 def embed_text(sentences, name, path, bert_model, batch_size, device, logger):
     logger.info(f'Getting {name} embeddings')
 
@@ -81,11 +62,11 @@ def embed_text(sentences, name, path, bert_model, batch_size, device, logger):
     sentences_to_embed = sentences.unique().tolist()
     tokenization = tokenize_text(sentences_to_embed, bert_model, batch_size)
     bert = torch.nn.DataParallel(AutoModel.from_pretrained(bert_model)).to(device)
+    embs = []
     with torch.no_grad():
-        embs = []
         for batch in tqdm(tokenization, desc='embedding', dynamic_ncols=True):
             embs.append(bert(**batch).last_hidden_state[:, 0].detach().cpu().numpy())
-        embeddings = np.concatenate(embs)
+    embeddings = np.concatenate(embs)
     mapping = {i: emb.tolist() for i, emb in zip(sentences_to_embed, embeddings)}
     result = sentences.map(mapping)
     logger.info('Saving calculated embeddings')
@@ -95,9 +76,8 @@ def embed_text(sentences, name, path, bert_model, batch_size, device, logger):
 
 def tokenize_text(sentences, bert_model, batch_size):
     tokenizer = AutoTokenizer.from_pretrained(bert_model, strip_accents=True)
-    num_samples = len(sentences)
-    token_batches = [sentences[j * batch_size:(j + 1) * batch_size] for j in range(num_samples // batch_size)] + \
-                    [sentences[(num_samples // batch_size) * batch_size:]]
+    token_batches = [sentences[j:j + batch_size] for j in range(0, len(sentences), batch_size)]
+
     if len(token_batches[-1]) == 0:
         token_batches = token_batches[:-1]
     tokenization = []
@@ -108,3 +88,14 @@ def tokenize_text(sentences, bert_model, batch_size):
                                       truncation=True,
                                       max_length=512))
     return tokenization
+
+
+def profile(func):
+    def wrapper(*args, **kwargs):
+        profiler = cProfile.Profile()
+        profiler.enable()
+        func(*args, **kwargs)
+        profiler.disable()
+        stats = pstats.Stats(profiler).sort_stats('cumtime')
+        stats.print_stats()
+    return wrapper