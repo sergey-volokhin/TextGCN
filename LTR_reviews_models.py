tqdm.auto; remove logger from embed; fix bug with item_emb[item] in score; REMOVE SIGMOID & ADD NORMALIZATION

LTR_reviews_models.py
@@ -1,10 +1,10 @@
 import numpy as np
 import pandas as pd
 import torch
-from tqdm import tqdm
+from tqdm.auto import tqdm
 
 from kg_models import DatasetKG
-from reviews_models import DatasetReviews, TextModelReviews
+from reviews_models import DatasetReviews
 from base_model import BaseModel
 
 
@@ -23,7 +23,7 @@ class LTRDataset(DatasetKG, DatasetReviews):
             user_text_embs).values.tolist()).to(self.device)
 
 
-class LTR(TextModelReviews):
+class LTR(BaseModel):
 
     def __init__(self, args, dataset):
         super().__init__(args, dataset)
@@ -38,16 +38,17 @@ class LTR(TextModelReviews):
     def _copy_dataset_args(self, dataset):
         super()._copy_dataset_args(dataset)
         self.users_as_avg_reviews = dataset.users_as_avg_reviews
+        self.items_as_avg_reviews = dataset.items_as_avg_reviews
         self.items_as_desc = dataset.items_as_desc
 
     def score_reviews(self, users, items, users_emb, item_emb):
         user_part = torch.cat([users_emb, self.users_as_avg_reviews[users]], axis=1)
-        item_part = torch.cat([item_emb[items], self.items_as_avg_reviews[items]], axis=1)
+        item_part = torch.cat([item_emb, self.items_as_avg_reviews[items]], axis=1)
         return torch.sum(torch.mul(user_part, item_part), dim=1)
 
     def score_kg(self, users, items, users_emb, item_emb):
         user_part = torch.cat([users_emb, self.users_as_avg_reviews[users]], axis=1)
-        item_part = torch.cat([item_emb[items], self.items_as_desc[items]], axis=1)
+        item_part = torch.cat([item_emb, self.items_as_desc[items]], axis=1)
         return torch.sum(torch.mul(user_part, item_part), dim=1)
 
 

base_model.py
@@ -8,7 +8,7 @@ import torch.optim as opt
 from tensorboardX import SummaryWriter
 from torch import nn
 from torch.nn import functional as F
-from tqdm import tqdm, trange
+from tqdm.auto import tqdm, trange
 
 from utils import early_stop, hit, ndcg, precision, recall
 
@@ -270,7 +270,7 @@ class BaseModel(nn.Module):
 
                 # get the estimated user-item scores with matmul embedding matrices
                 batch_user_emb = users_emb[torch.tensor(batch_users).long().to(self.device)]
-                rating = torch.sigmoid(torch.matmul(batch_user_emb, items_emb.t()))
+                rating = torch.matmul(batch_user_emb, items_emb.t())
 
                 # set scores for train items to be -inf so we don't recommend them
                 exclude_index, exclude_items = [], []

text_base_model.py
@@ -19,7 +19,7 @@ class TextBaseModel(BaseModel):
             'cosine_ratio': lambda x, y: torch.nan_to_num(1 / F.cosine_similarity(x, y),
                                                           posinf=0,
                                                           neginf=0),
-        }[self.distance]
+        }[self.dist_fn]
 
     def bpr_loss(self, users, pos, negs):
         users_emb, item_emb = self.representation
@@ -61,8 +61,8 @@ class TextBaseModel(BaseModel):
 
     def gnn_dist(self, pos, neg):
         ''' calculate similarity between gnn representations of the sampled items '''
-        cands = self.embedding_item(pos)
-        refs = self.embedding_item(neg)
+        cands = F.normalize(self.embedding_item(pos))
+        refs = F.normalize(self.embedding_item(neg))
         return self.dist_fn(cands, refs).to(self.device)
 
     def bert_dist(self, users, pos, neg):

utils.py
@@ -7,7 +7,7 @@ import random
 import numpy as np
 import torch
 from sentence_transformers import SentenceTransformer
-from tqdm import tqdm
+from tqdm.auto import tqdm
 from transformers import AutoModel, AutoTokenizer
 
 
@@ -113,9 +113,8 @@ def profile(func):
     return wrapper
 
 
-def embed_text(sentences, path, bert_model, batch_size, device, logger):
+def embed_text(sentences, path, bert_model, batch_size, device):
     ''' calculate SentenceBERT embeddings'''
-    logger.info('Getting embeddings')
 
     if os.path.exists(path):
         return torch.load(path, map_location=device)
@@ -132,6 +131,5 @@ def embed_text(sentences, path, bert_model, batch_size, device, logger):
 
     mapping = {i: emb for i, emb in zip(sentences_to_embed, embeddings)}
     result = torch.from_numpy(np.stack(sentences.map(mapping).values)).to(device=device)
-    logger.info('Saving calculated embeddings')
     torch.save(result, path)
     return result