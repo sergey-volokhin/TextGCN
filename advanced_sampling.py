029307b4bbe32c83c4e6b4677eddb8a0aecb64e4
S <s>
2022-05-20 03:52:05 -0400

    rename

--- advanced_sampling.py
+++ advanced_sampling.py
@@ -8,7 +8,7 @@ from dataset import BaseDataset
 class AdvSamplDataset(BaseDataset):
 
     def __getitem__(self, idx):
-        return idx // self.bucket_len, random.sample(self.range_items, k=1000)
+        return idx // self.bucket_len, random.sample(self.range_items, k=min(len(self.range_items), 1000))
 
 
 class AdvSamplModel(BaseModel):
@@ -22,19 +22,20 @@ class AdvSamplModel(BaseModel):
         super()._copy_dataset_args(dataset)
         self.positive_lists = dataset.positive_lists
 
-    def rank_items_for_batch_pairwise(self, users_emb, items_emb, batch_users):
+    def score_batchwise(self, users_emb, items_emb, users):
         ''' calculate scores for all items for users in the batch '''
         return torch.matmul(users_emb.unsqueeze(1), items_emb.transpose(1, 2)).squeeze()
 
     def get_loss(self, data):
         '''
-            rank all items from user-batch, select negative items from top k
+            rank all items from user-batch, select top-k negative items
             compute loss for them crossprod 5 positive per user
         '''
+
         users_emb, items_emb = self.representation
         users, items = data
         items = torch.stack(items).t()
-        rankings = self.rank_items_for_batch_pairwise(users_emb[users], items_emb[items], users)
+        rankings = self.score_batchwise(users_emb[users], items_emb[items], users)
         batch = []
         for user, item, rank in zip(users, items, rankings):
 
@@ -51,4 +52,5 @@ class AdvSamplModel(BaseModel):
         return super().get_loss(torch.cat(batch))
 
     def subtract_tensor_as_set(self, t1, t2):
+        ''' copied from stackoverflow. no clue how this works '''
         return t1[(t2.repeat(t1.shape[0], 1).T != t1).T.prod(1) == 1]
