773a24656cb0b255df43bcb4bc505889f760e108
S <s>
2023-12-03 02:23:24 -0500

    redo metrics dict

--- TextGCN/utils.py
+++ TextGCN/utils.py
@@ -6,8 +6,6 @@ import pstats
 import numpy as np
 import torch
 from sentence_transformers import SentenceTransformer
-from tqdm.auto import tqdm
-from transformers import AutoTokenizer
 
 
 def hit(row):
@@ -35,33 +33,42 @@ def ndcg(row, k):
     return dcg(rel, k) / idcg
 
 
-def calculate_metrics(df, metrics, ks):
-    ''' computes all metrics for predictions for all users '''
-    result = {i: [] for i in metrics}
+def calculate_metrics(df, ks):
+    '''
+    computes all metrics for predictions for all users
+    returns a dict of dicts with metrics for each k:
+        {k: {metric: value}}
+    '''
+
+    result = {}
     df['y_true_len'] = df['y_true'].apply(len)
 
-    ''' calculate intersections of y_pred and y_test '''
     for col in df.columns:
         df[col] = df[col].apply(np.array)
 
     for k in sorted(ks):
+        ''' calculate intersections of y_pred and y_test '''
         df[f'intersection_{k}'] = df.apply(lambda row: np.intersect1d(row['y_pred'][:k], row['y_true']), axis=1)
         df[f'y_pred_{k}'] = df['y_pred'].apply(lambda x: x[:k])
         df['intersecting_len'] = df[f'intersection_{k}'].apply(len)
+
         rec = recall(df)
         prec = precision(df, k)
-        result['recall'].append(rec.mean())
-        result['precision'].append(prec.mean())
-        result['hit'].append(hit(df).mean())
-        result['ndcg'].append(ndcg(df, k).mean())
         numerator = rec * prec * 2
         denominator = rec + prec
-        result['f1'].append(np.divide(
-            numerator,
-            denominator,
-            out=np.zeros_like(numerator),
-            where=denominator != 0,
-        ).mean())
+
+        result[k] = {
+            'recall': rec.mean(),
+            'precision': prec.mean(),
+            'hit': hit(df).mean(),
+            'ndcg': ndcg(df, k).mean(),
+            'f1': np.divide(
+                numerator,
+                denominator,
+                out=np.zeros_like(numerator),
+                where=denominator != 0,
+            ).mean()
+        }
     return result
 
 
@@ -84,11 +91,9 @@ def early_stop(res):
      the difference between metrics from current and 2 previous epochs is less than 1e-4
      or the last 3 epochs are yielding strictly declining values for all metrics
     '''
-    if len(res['recall']) < 3:
-        return False
-    declining = all(np.less(m[-1], m[-2]).all() and np.less(m[-2], m[-3]).all() for m in res.values())
-    converged = all(np.allclose(m[-1], m[-2], atol=1e-4) for m in res.values()) and \
-                all(np.allclose(m[-1], m[-3], atol=1e-4) for m in res.values())
+    declining = all(value[-1] < value[-2] < value[-3] for k in res for value in res[k].values())
+    converged = all(abs(value[-1] - value[-2]) < 1e-4 and abs(value[-3] - value[-2]) < 1e-4
+                    for k in res for value in res[k].values())
     return converged or declining
 
 
@@ -104,8 +109,8 @@ def embed_text(
     if os.path.exists(path):
         return torch.load(path, map_location=device)
 
-    def dedup_and_sort(line):
-        return sorted(line.unique().tolist(), key=lambda x: len(x.split(" ")), reverse=True)
+    def dedup_and_sort(line):  # sort by num tokens, split collisions by lexicographical order
+        return sorted(line.unique().tolist(), key=lambda x: (len(x.split()), x), reverse=True)
 
     os.makedirs(os.path.dirname(path), exist_ok=True)
     model = SentenceTransformer(bert_model, device=device)
