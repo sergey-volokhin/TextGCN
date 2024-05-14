2bf6e567547d7b65428303166ac0075c561e2c53
S <s>
2024-05-14 13:30:54 -0400

    separate salesforce model from sentence-transformers; sort sentences for embedding by length

--- src/utils.py
+++ src/utils.py
@@ -10,6 +10,7 @@ import torch
 from more_itertools import chunked
 from torch.nn import functional as F
 from tqdm.auto import tqdm
+from transformers import AutoModel, BitsAndBytesConfig, AutoTokenizer
 
 
 def timeit(func):
@@ -103,11 +104,20 @@ def get_logger(config):
     return logging.getLogger()
 
 
-def num_tokens_from_list(strings: list[str], encoding_name="cl100k_base") -> int:
-    """Returns the number of tokens in a text string."""
-    encoding = tiktoken.get_encoding(encoding_name)
-    num_tokens = sum([len(encoding.encode(string)) for string in strings])
-    return num_tokens
+def sort_process_unsort(func):
+    ''' Decorator to sort the input by length, process it, and then unsort the output '''
+    @wraps(func)
+    def wrapper(sentences, **kwargs):
+        sorted_strings_with_indices = sorted(enumerate(sentences), key=lambda x: -len(x[1]))
+        sorted_strings = [string for _, string in sorted_strings_with_indices]
+        processed_values = func(sentences=sorted_strings, **kwargs)
+        unsorted_values = [None] * len(sentences)
+        for (original_index, _), value in zip(sorted_strings_with_indices, processed_values):
+            unsorted_values[original_index] = value
+        if isinstance(processed_values, torch.Tensor):
+            return torch.stack(unsorted_values)
+        return unsorted_values
+    return wrapper
 
 
 @timeit
@@ -135,10 +145,12 @@ def embed_text(
     logger.info(f'embedding {len(to_embed)} sentences')
     if model_name.startswith('text-embedding-3'):
         result = embed_openai(to_embed, model_name, dimensions)
-    elif model_name.startswith('all-') or model_name in ['Salesforce/SFR-Embedding-Mistral']:
+    elif model_name.startswith('all-'):  # or model_name in ['Salesforce/SFR-Embedding-Mistral']:
         from sentence_transformers import SentenceTransformer
         model = SentenceTransformer(model_name, device=device)
         result = model.encode(to_embed, batch_size=batch_size, convert_to_tensor=True)
+    elif model_name in ['Salesforce/SFR-Embedding-Mistral']:
+        result = embed_salesforce(to_embed, model_name=model_name, batch_size=batch_size)
     else:
         raise ValueError(f'Unknown encoder: {model_name}')
 
@@ -152,7 +164,15 @@ def embed_text(
 
 
 def embed_openai(sentences, model_name, dimensions):
-    import openai, tiktoken
+    import openai
+    import tiktoken
+
+    def num_tokens_from_list(strings: list[str], encoding_name="cl100k_base") -> int:
+        """Returns the number of tokens in a text string."""
+        encoding = tiktoken.get_encoding(encoding_name)
+        num_tokens = sum([len(encoding.encode(string)) for string in strings])
+        return num_tokens
+
     client = openai.OpenAI()
     batches = list(chunked(sentences, 2000))
     lengths = [num_tokens_from_list(batch) for batch in batches]
@@ -162,6 +182,44 @@ def embed_openai(sentences, model_name, dimensions):
         embeddings += [i.embedding for i in client.embeddings.create(input=batch, model=model_name, dimensions=dimensions).data]
         time.sleep(1)
     return torch.tensor(embeddings)
+
+
+@sort_process_unsort
+@torch.no_grad()
+def embed_salesforce(sentences, model_name, batch_size):
+    bnb_config = BitsAndBytesConfig(
+        load_in_4bit=True,
+        bnb_4bit_use_double_quant=True,
+        bnb_4bit_quant_type="nf4",
+        bnb_4bit_compute_dtype=torch.bfloat16
+    )
+    model = AutoModel.from_pretrained(
+        model_name,
+        trust_remote_code=True,
+        device_map='auto',
+        torch_dtype=torch.bfloat16,
+        quantization_config=bnb_config
+    )
+
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
+
+    def last_token_pool(last_hidden_states, attention_mask):
+        left_padding = (attention_mask[:, -1].sum() == attention_mask.shape[0])
+        if left_padding:
+            return last_hidden_states[:, -1]
+        else:
+            sequence_lengths = attention_mask.sum(dim=1) - 1
+            batch_size = last_hidden_states.shape[0]
+            return last_hidden_states[torch.arange(batch_size, device=last_hidden_states.device), sequence_lengths]
+
+    all_embeddings = []
+    batches = list(chunked(sentences, batch_size))
+    for batch in tqdm(batches, desc="Salesforce batches"):
+        batch_dict = tokenizer(batch, max_length=4096, padding=True, truncation=True, return_tensors="pt")
+        outputs = model(**batch_dict)
+        batch_embeddings = last_token_pool(outputs.last_hidden_state, batch_dict['attention_mask'])
+        all_embeddings.append(batch_embeddings)
+    return torch.cat(all_embeddings, dim=0)
 
 
 def train_test_split_stratified(df, column='user_id', train_size=0.8, seed=42):
