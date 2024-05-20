aeafcb77a722eea72ff719200ca6a9443209bd79
Sergey Volokhin <svolokh@emory.edu>
2024-05-20 18:26:13 -0400

    better ranking train test split

--- src/DatasetRanking.py
+++ src/DatasetRanking.py
@@ -32,4 +32,24 @@ class DatasetRanking(BaseDataset):
         self.true_test_lil = self.test_df.groupby('user_id')['asin'].aggregate(list).values.tolist()
 
     def _train_test_split(self, df, train_size=0.8):
-        self.train_df, self.test_df = tts(df, train_size=train_size, stratify=df['user_id'], random_state=self.seed)
+        '''
+        split users into two groups: those with 2 reviews and those with more than 2 reviews
+        ensure that each user is in both train and test set by selecting at least one review per user
+        '''
+
+        vc = df.user_id.value_counts()
+        two_reviews = df[df.user_id.isin(vc[vc == 2].keys())]
+        more_than_two = df.drop(two_reviews.index)
+
+        train_two = two_reviews.groupby('user_id').sample(n=1, random_state=self.seed)
+        test_two = two_reviews[~two_reviews.index.isin(train_two.index)]
+
+        train_more_than_two, test_more_than_two = tts(
+            more_than_two,
+            train_size=train_size,
+            random_state=self.seed,
+            stratify=more_than_two.user_id,
+        )
+
+        self.train_df = pd.concat([train_two, train_more_than_two])
+        self.test_df = pd.concat([test_two, test_more_than_two])
