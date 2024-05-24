70ff3c0f87ebdfcc61d3fcaf953232c0c49d0798
Sergey Volokhin <svolokh@emory.edu>
2024-05-24 19:53:30 -0400

    redo user_profile and llama, modularize, clean up, move prompts into file, remove unused things, use short model names, fix profile caching

--- user_profile.py
+++ user_profile.py
@@ -1,30 +1,34 @@
-import numpy as np
 import argparse
 import json
 import os
 import random
 
 import pandas as pd
-from transformers import AutoTokenizer
 from tqdm.auto import tqdm
+from transformers import AutoTokenizer
 
-
-from llama import call_pipe, clean_text_series, get_pipe, num_tokens, timeit
+from llama import (
+    call_pipe,
+    call_pipe_dummy,
+    clean_text_series,
+    cut_to,
+    get_pipe,
+    longer_names,
+    num_tokens,
+    num_tokens_conversation,
+    timeit,
+)
 
 pd.options.display.width = 0
 
 template = '<Item Title>: "{}"; <Item Description>: "{}"; <User Review> "{}"; <User Score>: {}'
 
 
-def cut_to(sentence, tokenizer, max_length):
-    return tokenizer.convert_tokens_to_string(tokenizer.tokenize(sentence)[:max_length])
-
-
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--model_name', type=str, default='meta-llama/Llama-2-7b-chat-hf')
+    parser.add_argument('--model_name', type=str, default='llama_3', choices=['llama_2', 'llama_3'])
     parser.add_argument('--data', '-d', type=str, default='data/sampled_Toys_and_Games')
-    parser.add_argument('--temp', type=float, default=0.01)
+    parser.add_argument('--temp', type=float, default=0.4)
     parser.add_argument(
         '--min_new_tokens',
         type=int,
@@ -51,22 +55,19 @@ def parse_args():
     )
     parser.add_argument('--batch_size', type=int, default=8)
     parser.add_argument('--seed', type=int, default=1)
-    parser.add_argument('--prompt_path', type=str, default='prompts/prompts_proper.json')
+    parser.add_argument('--prompts_template_path', type=str, default='prompts/prompts_proper.json')
     parser.add_argument('--regenerate', action='store_true', help='regenerate prompts')
     args = parser.parse_args()
-    if 'llama-2' in args.model_name.lower():
-        args.max_length = 4096
-    elif 'llama-3' in args.model_name.lower():
-        # args.max_length = 4096
-        args.max_length = 8192
-    else:
-        raise ValueError(f'model not llama: {args.model_name}')
+    args.max_length = {
+        'llama_2': 4096,
+        'llama_3': 8192,
+    }[args.model_name.lower()]
     return args
 
 
 @timeit
 def clean_data(args):
-    tokenizer = AutoTokenizer.from_pretrained(args.model_name)
+    tokenizer = AutoTokenizer.from_pretrained(longer_names[args.model_name])
 
     reviews = pd.read_table(f'{args.data}/reviews_text.tsv').dropna()
     reviews['review'] = clean_text_series(reviews.review).apply(cut_to, args=(tokenizer, args.max_review_length))
@@ -98,85 +99,100 @@ def load_data(args):
     return train.merge(meta, on='asin').merge(reviews, on=['user_id', 'asin', 'rating'])
 
 
-def process_group(group, prompt_len, args, prompt, tokenizer):
+def process_group(group, prompt_length, max_prompt_length, prompt_template, tokenizer):
+    '''append reviews one at a time until the prompt is too long'''
     filler = ''
-    current_len = prompt_len
+    current_len = prompt_length
     for ind, row in enumerate(group.itertuples(), start=1):
         # control length of the final prompt by breaking early
         to_add = f'{ind}. ' + template.format(row.title, row.description, row.review, row.rating) + '\n'
         filler += to_add
         current_len += num_tokens(to_add, tokenizer)
-        if current_len > args.max_length - args.max_new_tokens:
+        if current_len > max_prompt_length:
             break
-    return [{k: v.format(filler) if k == 'content' else v for k, v in p.items()} for p in prompt]
+    return str([{k: v.format(filler) if k == 'content' else v for k, v in p.items()} for p in prompt_template])
 
 
 def load_cache(path):
-    if os.path.exists(os.path.join(path, 'embeddings/profiles_cache.tsv')):
-        return (
-            pd.read_table(os.path.join(path, 'embeddings/profiles_cache.tsv'))
-            .set_index('prompt_profile')['profile']
-            .to_dict()
-        )
+    '''
+    df with [prompt, profile]
+    profile doesn't depend on model b.c. prompts are different
+    '''
+    if os.path.exists(path):
+        return pd.read_table(path).set_index('prompt_profile')['profile'].to_dict()
+    print('profile cache not found, creating')
     return {}
 
 
-def load_prompt(path, tokenizer):
+def load_prompt_template(path):
     with open(path, 'r') as file:
-        prompt = json.load(file)['user_profile']
-    prompt_length = sum(
-        num_tokens(value, tokenizer)
-        for turn in prompt
-        for key, value in turn.items()
-        if key == 'content'
-    )
-    return prompt, prompt_length
+        return json.load(file)['user_prompts']['user_profile']
+
+
+def construct_prompts(df, args, prompt_template, tokenizer):
+    path = f'{args.data}/reshuffle_{args.seed}/prompts_{args.model_name}.tsv'  # prompts don't depend on temp
+
+    if os.path.exists(path) and not args.regenerate:
+        return pd.read_table(path, usecols=['user_id', 'prompt_profile'])
+
+    prompts_df = []
+    prompt_length = num_tokens_conversation(prompt_template, tokenizer)
+    for user, group in tqdm(df.groupby('user_id'), dynamic_ncols=True):
+        prompts_df.append(
+            (
+                user,
+                process_group(
+                    group,
+                    prompt_length=prompt_length,
+                    max_prompt_length=args.max_length - args.max_new_tokens,
+                    prompt_template=prompt_template,
+                    tokenizer=tokenizer,
+                ),
+            )
+        )
+    prompts_df = pd.DataFrame(prompts_df, columns=['user_id', 'prompt_profile']).set_index('user_id')
+    print(f'saving filled prompts into {path}')
+    # prompts_df.to_csv(path, sep='\t')
+    return prompts_df
 
 
 @timeit
-def generate_profile(pipe, tokenizer, data, args):
+def generate_profile(data, args, pipe):
+    '''
+    profiles file format:            v each model has own column
+        user_id \t prompt_profile \t profile_{model_name}
+                   ^ list of dicts so the same for all models
+    '''
 
-    prompt, prompt_length = load_prompt(args.prompt_path, tokenizer)
-    cache = load_cache(args.data)
+    profile_path = f'{args.data}/reshuffle_{args.seed}/profiles_{args.temp}_{args.model_name}.tsv'
 
-    if 'llama-3' in args.model_name.lower():
-        profile_path = f'{args.data}/reshuffle_{args.seed}/profiles_{args.temp}_llama3.tsv'
-    elif 'llama-2' in args.model_name.lower():
-        profile_path = f'{args.data}/reshuffle_{args.seed}/profiles_{args.temp}_llama2.tsv'
-    else:
-        raise ValueError(f'model not llama: {args.model_name}')
+    cache_path = f'{args.data}/profiles_cache_{args.temp}_{args.model_name}.tsv'
+    cache = load_cache(cache_path)
 
-    # construct and fill prompts
-    if os.path.exists(profile_path) and not args.regenerate:
-        descriptions = pd.read_table(profile_path)
-    else:
-        descriptions = []
-        for user, group in tqdm(data.groupby('user_id'), dynamic_ncols=True):
-            descriptions.append((user, process_group(group, prompt_length, args, prompt, tokenizer)))
-        descriptions = pd.DataFrame(descriptions, columns=['user_id', 'prompt_profile']).set_index('user_id')
-        descriptions.to_csv(profile_path, sep='\t')
+    prompt_template = load_prompt_template(args.prompts_template_path)
+    prompts_df = construct_prompts(data, args, prompt_template, pipe.tokenizer)
 
     # select only the prompts that need to be generated
-    descriptions['profile'] = descriptions['prompt_profile'].map(lambda x: cache.get(x, np.nan))
-    to_generate = descriptions[descriptions['profile'].isna()].prompt_profile.tolist()
-    print('len(to_generate)', len(to_generate), 'instead of', len(descriptions))
+    prompts_df['profile'] = prompts_df['prompt_profile'].map(cache)
+    to_generate = prompts_df[prompts_df['profile'].isna()].prompt_profile.apply(eval).tolist()
+    print(f'caching saved us {100 * (1 - len(to_generate) / len(prompts_df)):.1f}%, generating for {len(to_generate)}')
+    # generate the prompts and put back into dataframe
     generated = call_pipe(
         pipe=pipe,
         queries=to_generate,
         batch_size=args.batch_size,
     )
-    descriptions['profile'] = descriptions['profile'].astype(object)  # dunno why this is needed
-    descriptions.loc[descriptions['profile'].isna(), 'profile'] = generated
-    descriptions.to_csv(profile_path, sep='\t', index=False)
+    prompts_df['profile'] = prompts_df['profile'].astype(object)  # dunno why this is needed
+    prompts_df.loc[prompts_df['profile'].isna(), 'profile'] = generated
+    prompts_df.to_csv(profile_path, sep='\t', index=False)
+    print(f'save profiles into {profile_path}')
 
-    cache.update(descriptions.set_index('prompt_profile')['profile'].to_dict())
-    pd.DataFrame(cache.items(), columns=['prompt_profile', 'profile']).to_csv(
-        os.path.join(args.data, 'embeddings/profiles_cache.tsv'),
-        sep='\t',
-        index=False,
-    )
+    # update cache
+    cache.update(prompts_df.set_index('prompt_profile')['profile'].to_dict())
+    pd.DataFrame(cache.items(), columns=['prompt_profile', 'profile']).to_csv(cache_path, sep='\t', index=False)
+    print(f'saving cached profiles into {cache_path}')
 
-    return descriptions
+    return prompts_df
 
 
 def main():
@@ -184,7 +200,7 @@ def main():
     random.seed(args.seed)
     pipe = get_pipe(args, quantization='bfloat16')
     data = load_data(args)
-    profiles = generate_profile(pipe, pipe.tokenizer, data, args)
+    profiles = generate_profile(data, args, pipe)
 
 
 if __name__ == '__main__':
